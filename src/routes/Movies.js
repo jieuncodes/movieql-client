e78a236fda4797294b93bde696ff64fe169bc035
youredith <youredith3@gmail.com>
2023-10-15 17:11:24 +0900

    test: Check API by fetching Tweets

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -6,21 +6,36 @@ const ALL_MOVIES = gql`
       title
       id
     }
+    allTweets {
+      id
+      text
+      author {
+        fullName
+      }
+    }
   }
 `;
 
-function Movies() {
+export default function Movies() {
   const { data, loading, error } = useQuery(ALL_MOVIES);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error.message}</p>;
-
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch :(</h1>;
+  }
   return (
     <ul>
-      {data.allMovies.map((movie, index) => (
+      <h1>Movies</h1>
+      {data.allMovies.map((movie) => (
         <li key={movie.id}>{movie.title}</li>
+      ))}
+      <h1>Tweets</h1>
+      {data.allTweets.map((tweet) => (
+        <li key={tweet.id}>
+          {tweet.text}/by: {tweet.author.fullName}
+        </li>
       ))}
     </ul>
   );
 }
-export default Movies;
