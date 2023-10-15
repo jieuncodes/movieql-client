128fc325cf8827e2d8acd99421a34ce7091fa3b8
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-15 15:17:45 +0900

    feat: setup Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Movies from "./routes/Movies";
+import Movie from "./routes/Movie";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/Movies.js
@@ -0,0 +1,4 @@
+function Movies() {
+  return <div>This is Movies page</div>;
+}
+export default Movies;
