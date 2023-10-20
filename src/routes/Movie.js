0b25ffe6da6d700572cbbeace3604f555eeaeaae
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-20 19:14:58 +0900

    feat: movie page and query caching (Apollo cache)

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,4 +1,28 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
+
 function Movie() {
-  return <div>This is movie details</div>;
+  const { movieId } = useParams();
+  const { data, loading, error } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId,
+    },
+  });
+  if (loading) {
+    return <h1>Fetching Movie...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch Movie:{error.message}</h1>;
+  }
+  return <div>{data.movie.title}</div>;
 }
 export default Movie;
